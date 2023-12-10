f5b4cb447e2d4aaa111c9fef610f20f1f0c33207
Mark Anthony <touya1313@hotmail.com>
2023-12-09 23:58:18 -0600

    finished updating ids in js file

--- Assets/script.js
+++ Assets/script.js
@@ -38,16 +38,16 @@ function timeIndicator() {
 
 // Gets user input that was saved in local storage and sets the values of the textarea elements. 
 
-$("#9.description").val(localStorage.getItem("nineHour"));
-$("#10.description").val(localStorage.getItem("tenHour"));
-$("#11.description").val(localStorage.getItem("elevenHour"));
-$("#12.description").val(localStorage.getItem("twelveHour"));
-$("#13.description").val(localStorage.getItem("oneHour"));
-$("#14.description").val(localStorage.getItem("twoHour"));
-$("#15.description").val(localStorage.getItem("threeHour"));
-$("#16.description").val(localStorage.getItem("fourHour"));
-$("#17.description").val(localStorage.getItem("fiveHour"));
-$("#18.description").val(localStorage.getItem("sixHour"));
+$("#9.description").val(localStorage.getItem("9"));
+$("#10.description").val(localStorage.getItem("10"));
+$("#11.description").val(localStorage.getItem("11"));
+$("#12.description").val(localStorage.getItem("12"));
+$("#13.description").val(localStorage.getItem("13"));
+$("#14.description").val(localStorage.getItem("14"));
+$("#15.description").val(localStorage.getItem("15"));
+$("#16.description").val(localStorage.getItem("16"));
+$("#17.description").val(localStorage.getItem("17"));
+$("#18.description").val(localStorage.getItem("18"));
 
 timeIndicator();
 
