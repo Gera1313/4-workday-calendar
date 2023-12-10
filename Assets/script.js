5d32764d0f4481e685f09e7feed422be11ef870a
Mark Anthony <touya1313@hotmail.com>
2023-12-10 03:53:56 -0600

    replaced repeated code with a for loop

--- Assets/script.js
+++ Assets/script.js
@@ -55,16 +55,9 @@ function timeIndicator() {
 
 // Gets user input that was saved in local storage and sets the values of the textarea elements.
 
-$("#9.description").val(localStorage.getItem("9"));
-$("#10.description").val(localStorage.getItem("10"));
-$("#11.description").val(localStorage.getItem("11"));
-$("#12.description").val(localStorage.getItem("12"));
-$("#13.description").val(localStorage.getItem("13"));
-$("#14.description").val(localStorage.getItem("14"));
-$("#15.description").val(localStorage.getItem("15"));
-$("#16.description").val(localStorage.getItem("16"));
-$("#17.description").val(localStorage.getItem("17"));
-$("#18.description").val(localStorage.getItem("18"));
+for (let i = 9; i <= 18; i++) {
+    $(`#${i}.description`).val(localStorage.getItem(`${i}`));
+}
 
 timeIndicator();
 
