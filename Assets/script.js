187447b52e5610286a32120cfb1676af8c930ecf
Mark Anthony <touya1313@hotmail.com>
2023-12-10 02:50:41 -0600

    ah it works! it saves text after refreshing now

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,59 @@
 // Save data to local storage before the page is unloaded
 
 $(window).on("beforeunload", function () {
-    // Save user input to local storage
+  // Save user input to local storage
+  $(".time-block").each(function () {
+    let id = $(this).attr("id");
+    let text = $(this).find(".description").val();
+
+    localStorage.setItem(id, text);
+  });
+});
+
+//   Load data from local storage when the document is ready
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+});
+
+// Gets user input that was saved in local storage and sets the values of the textarea elements.
+
     $(".time-block").each(function () {
         let id = $(this).attr("id");
-        let text = $(this).find(".description").val();
-  
-        localStorage.setItem(id, text);
+        let savedText = localStorage.getItem(id);
+        $(this).find(".description").val(savedText);
     });
-  });
 
-//  Applies the past, present, or future class to each time block by comparing the id to the current hour. 
+//  Applies the past, present, or future class to each time block by comparing the id to the current hour.
 
 function timeIndicator() {
-    let currentTime = dayjs().hour();
+  let currentTime = dayjs().hour();
 
-    $(".time-block").each(function () {
-        let timeBlockEl = parseInt($(this).attr("id").split("hour")[1]);
+  $(".time-block").each(function () {
+    let timeBlockEl = parseInt($(this).attr("id").split("hour")[1]);
 
-        if (timeBlockEl > currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
-        else if (timeBlockEl === currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-            $(this).addClass("past");
-
-        }
-    })
+    if (timeBlockEl > currentTime) {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (timeBlockEl === currentTime) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
 }
 
-// Gets user input that was saved in local storage and sets the values of the textarea elements. 
+// Gets user input that was saved in local storage and sets the values of the textarea elements.
 
 $("#9.description").val(localStorage.getItem("9"));
 $("#10.description").val(localStorage.getItem("10"));
@@ -54,5 +70,5 @@ timeIndicator();
 
 // code to display current date in the header
 
-let today = dayjs().format('MMM D, YYYY, dddd, hh:mm a');
-$('#currentDay').text("Today is " + today)
+let today = dayjs().format("MMM D, YYYY, dddd, hh:mm a");
+$("#currentDay").text("Today is " + today);
